864933038fe0149a60bf30bc90f9a259d6e7037d
LDalby <lars@bios.au.dk>
2014-10-28 11:32:36 +0100

    Add projection functionality

--- R/CalcPseudoCentroid.r
+++ R/CalcPseudoCentroid.r
@@ -4,12 +4,12 @@
 #' Be aware that this is only an approximation. With funny shaped polygons 
 #' you might get strange results. Particularly if the vertices are not
 #' are not evenly spaced. 
-#' 
 #' @param data data.frame The raw file from the bird database
+#' @param project logical Should the input coordinates be projected to UTM?
 #' @return data.frame A data.frame with the polygon id and the pseudo centroid
 #' @export
 
-CalcPseudoCentroid = function(data)
+CalcPseudoCentroid = function(data, project = TRUE)
 {
 	data$PolygonData = str_replace_all(data[,PolygonData], "\\(", "")
 	data$PolygonData = str_replace_all(data[,PolygonData], "\\)", "")
@@ -24,5 +24,16 @@ CalcPseudoCentroid = function(data)
 	polygons = lapply(polygons, FUN = GetMean)
 	polygons = unlist(polygons)
 	polygons = data.frame('PolyID' = data[,PolyID], 'long' = polygons[which(1:length(polygons) %% 2 != 0)], 'lat' = polygons[which(1:length(polygons) %% 2 == 0)])
+
+	if(project)
+	{
+		sp.polygons = polygons
+		coordinates(sp.polygons) = ~long+lat
+		proj4string(sp.polygons) = CRS('+proj=longlat +datum=WGS84')
+		sp.polygons = spTransform(sp.polygons, CRS('+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'))
+        polygons$UtmX = sp.polygons@coords[,1]
+        polygons$UtmY = sp.polygons@coords[,2]
+	}
 	return(polygons)
 }
+
