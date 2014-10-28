ac0e072de070ca965898cb567c3a45e4b48fffaf
LDalby <lars@bios.au.dk>
2014-10-28 09:08:24 +0100

    update function - potentially working now.

--- R/CalcPseudoCentroid.r
+++ R/CalcPseudoCentroid.r
@@ -2,27 +2,27 @@
 #'
 #' Calculate the mean x and mean y coordinate for a number of polygons
 #' Be aware that this is only an approximation. With funny shaped polygons 
-#' you might get strange results.
+#' you might get strange results. Particularly if the vertices are not
+#' are not evenly spaced. 
 #' 
-#' @param polygons data.frame The raw file from the bird database
-#' @return The pseudo centroid
+#' @param data data.frame The raw file from the bird database
+#' @return data.frame A data.frame with the polygon id and the pseudo centroid
 #' @export
 
-data = fread('o:/ST_GooseProject/Field data/Fugledata/fugledata_polygoner_20141027.csv')
+CalcPseudoCentroid = function(data)
+{
+	data$PolygonData = str_replace_all(data[,PolygonData], "\\(", "")
+	data$PolygonData = str_replace_all(data[,PolygonData], "\\)", "")
+	polygons = str_split(data[,PolygonData], ',')
+	polygons = lapply(polygons, FUN = str_trim)
+	polygons = lapply(polygons, FUN = as.numeric)
 
-data$PolygonData = str_replace_all(data[,PolygonData], "\\(", "")
-data$PolygonData = str_replace_all(data[,PolygonData], "\\)", "")
-poly = data[PolyID == 11]
-poly = poly$PolygonData
-temp = str_split(data[,PolygonData], ',')[[1]]
+		GetMean = function(x){
+			return(c(mean(x[which(1:length(x) %% 2 == 0)]), mean(x[which(1:length(x) %% 2 != 0)])))
+		}
 
-temp[which(1:length(temp) %% 2 != 0)]  # Her skal der bare tages et mean - mere eller mindre
-temp[which(1:length(temp) %% 2 == 0)]  # Her skal der bare tages et mean - mere eller mindre
-
-
- fruits <- c("one apple", "two pears", "three bananas")
-
-
-CalcPseudoCentroid = function(data) {
-
+	polygons = lapply(polygons, FUN = GetMean)
+	polygons = unlist(polygons)
+	polygons = data.frame('PolyID' = data[,PolyID], 'long' = polygons[which(1:length(polygons) %% 2 == 0)], 'lat' = polygons[which(1:length(polygons) %% 2 != 0)]))
+	return(polygons)
 }
