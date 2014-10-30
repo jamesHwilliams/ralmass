285de716357e9c3fa821ca83345b7ee063e3758a
LDalby <lars@bios.au.dk>
2014-10-30 14:57:06 +0100

    Add subset argument to UtmToALMaSS
    And and .RHistory to gitignore

--- R/UtmToALMaSS.r
+++ R/UtmToALMaSS.r
@@ -10,10 +10,12 @@
 #' @param long character The name of the column holding the longitude
 #' @param lat character The name of the column holding the latitude
 #' @param map character The name of the map used in ALMaSS
+#' @param map logical Should the output only contain locations within the 
+#' specified map?
 #' @return data.frame A data.frame with the ALMaSS coordinates
 #' @export
 
-UtmToALMaSS = function(data, long, lat, map) {
+UtmToALMaSS = function(data, long, lat, map, subset = TRUE) {
 if(map == 'VejlerneTest' | map == 'VejlerneTest.lsb')
 {
 	x = 494505
@@ -23,8 +25,9 @@ if(map == 'VejlerneBigMap' | map == 'VejlerneBigMap.lsb') {
 	x = 484378
 	y = 6335161
 }	
-
 	data$ALong = floor(x-data[,long])
 	data$ALat = floor(y-data[,lat])
+	data = data[which(data$ALong >= 0 && data$ALong <= x),]
+	data = data[which(data$ALat >= 0 && data$ALat <= y),]
 	return(data)
 }
