491d6a23e07321142433de54d3f981c66f1a4af3
LDalby <lars@bios.au.dk>
2014-11-07 15:09:33 +0100

    Add from almass to utm feature

--- R/UtmToALMaSS.r
+++ R/UtmToALMaSS.r
@@ -10,12 +10,15 @@
 #' @param long character The name of the column holding the longitude
 #' @param lat character The name of the column holding the latitude
 #' @param map character The name of the map used in ALMaSS
-#' @param map logical Should the output only contain locations within the 
+#' @param subset logical Should the output only contain locations within the
+#' ALMaSS map? Ignored when toalmass = FALSE
+#' @param toalmass logical Do you want to convert to ALMaSS coordinates. Use FALSE if 
+#' if you want to convert ALMaSS map coordinates back to UTM 
 #' specified map?
 #' @return data.frame A data.frame with the ALMaSS coordinates
 #' @export
 
-UtmToALMaSS = function(data, long, lat, map, subset = TRUE) {
+UtmToALMaSS = function(data, long, lat, map, subset = TRUE, toalmass = TRUE) {
 if(map == 'VejlerneTest' | map == 'VejlerneTest.lsb')
 {
 	x = 494505
@@ -23,22 +26,43 @@ if(map == 'VejlerneTest' | map == 'VejlerneTest.lsb')
 	dimx = 10000
 	dimy = 7000
 }
-
 if(map == 'VejlerneBigMap' | map == 'VejlerneBigMap.lsb')
 {
 	x = 484378
 	y = 6335161
 	dimx = 36000
 	dimy = 24000
-}	
-
-data$ALong = floor(data[,long]-x)
-data$ALat = floor(y-data[,lat])
-
-if(subset) 
+}
+if(toalmass)
 {
-	data = data[which(data$ALong >= 0 & data$ALong <= dimx),]
-	data = data[which(data$ALat >= 0 & data$ALat <= dimy),]
+	data$ALong = floor(data[,long]-x)
+	data$ALat = floor(y-data[,lat])
+
+	if(subset) 
+	{
+		data = data[which(data$ALong >= 0 & data$ALong <= dimx),]
+		data = data[which(data$ALat >= 0 & data$ALat <= dimy),]
+	}
+	return(data)
 }
-return(data)
+
+if(!toalmass)
+{
+	if(is.character(long)) {
+		varname = as.name(long)
+		r = eval(varname, data)
+		data$ALong = floor(x + data[, r])
+	}
+	if(is.character(lat)) {
+		varname = as.name(lat)
+		r = eval(varname, data)
+		data$ALat = floor(y - data[, r])	
+	}
+	if(!is.character(long) & !is.character(lat)) {
+		data$ALong = floor(data[,long]+x)
+		data$ALat = floor(y-data[,latr])
+	}
+	return(data)
 }
+}
+
