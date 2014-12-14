3d0a96e52977dd5adec8a689ee44b3f0384d61c9
LDalby <lars@bios.au.dk>
2014-12-14 20:02:19 +0100

    Add new function to plot weather data

---
+++ R/PlotWeather.R
@@ -0,0 +1,32 @@
+#' Plot an Almass weather file
+#'
+#' Plot an Almass weather file or a subset of it 
+#' 
+#' @param file character The full path to the weather file
+#' @param subset numeric Either a year or a sequence of years. If left blank - plot the whole thing.
+#' @return A nice plot
+#' @export
+PlotWeather = function(file, subset = NULL){
+	weather = fread(file, skip = 1)
+	setnames(weather, c('Year', 'Month', 'Day', 'Temp', 'Wind', 'Rain'))
+	weather[,Julian:=ymd(paste(weather[,Year],weather[,Month],weather[,Day], sep = '-'))]
+	if(!is.null(subset)) 
+	{
+	if(min(subset) < min(weather[,Year]) | max(subset) > max(weather[,Year]))
+	{
+		return(cat('Subset out of range of the provided weather file\n'))
+	}
+		if(length(subset) > 1) 
+		{
+			weather = weather[year(Julian) >= min(subset) & year(Julian) <= max(subset)]
+		}
+		else
+		{
+			weather = weather[year(Julian) == subset]
+		}
+	}
+	par(mfrow = c(3,1), las = 1, bty = 'l', mar =  c(2, 4, 2, 1) + 0.1)
+	plot(weather[,Julian], weather[,Temp], type = 'l', ylab = 'Temperature')
+	plot(weather[,Julian], weather[,Rain], type = 'l', ylab = 'Rain')
+	plot(weather[,Julian], weather[,Wind], type = 'l', ylab = 'Wind')
+}
