cef1c7463435b5c48231bbaab82b240508ae0042
LDalby <lars@bios.au.dk>
2014-12-18 16:39:22 +0100

    Modify to make farmref files

--- R/WritePolyref.r
+++ R/WritePolyref.r
@@ -7,8 +7,15 @@
 #' @return A tab separated text file formatted according to the requirements for a polygon reference file for ALMaSS.
 #' @export
 
-WritePolyref = function(Table, PathToFile, Headers = TRUE){
-	Table[,`:=`(Openness = -1, CentroidX = -1, CentroidY = -1)]
+WritePolyref = function(Table, PathToFile, Headers = TRUE, Type = 'Poly'){
+	if(Type != 'Poly' & Type != 'Farm') 
+	{
+		cat('Oops! Looks like you are asking for stuff you cant get...')
+	}
+	if(Type == 'Poly')
+	{
+		Table[,`:=`(Openness = -1, CentroidX = -1, CentroidY = -1)]
+	}
 	filecon = file(PathToFile, open = 'wt')
 	cat(paste(nrow(Table)), '\n',  file = filecon)
 	write.table(Table, file = filecon, sep = '\t', append = TRUE, row.names = FALSE, col.names = Headers, quote = FALSE)
