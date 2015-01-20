26c73e526f73f3c3fdabb95e14c689c1f7e5040c
LDalby <lars.dalby>
2015-01-20 20:58:54 +0100

    Add function to convert sim day to date

---
+++ R/ConvertSimDay.r
@@ -0,0 +1,11 @@
+#' Supply the calendar date from a sim day
+#'
+#' Return the calendar date from a simulation day number
+#' 
+#' @param DayNunber integer A day number
+#' @return The calendar date
+#' @export
+ConvertSimDay = function(DayNumber, Origin = '1990-01-01') {
+	origin = '1990-01-01'
+    return(ymd(origin) + days(DayNumber))
+}
