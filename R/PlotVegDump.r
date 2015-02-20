32d0136a4bf3e8bb083b89b9b85f5b86bab5d188
LDalby <lars@bios.au.dk>
2015-02-20 11:08:32 +0100

    Add theme

--- R/PlotVegDump.r
+++ R/PlotVegDump.r
@@ -15,7 +15,7 @@
 PlotVegDump = function(data, drop = TRUE) {
 start = 0
 end = 365
-by = 25
+by = 50
 if(drop) 
 {
 data = data[Year > 0]
@@ -25,18 +25,19 @@ yrng = range(data[,Height])
 xrng = range(data[Grazed == 1,Day])
 
 scale = scale_x_continuous(breaks = seq(start, end, by = by)) 
+theme = theme_bw()
 
-h = ggplot(data, aes(Day, Height)) + geom_line() + scale 
+h = ggplot(data, aes(Day, Height)) + geom_line() + scale + theme 
 h = h + annotate('rect', xmin = xrng[1], xmax = xrng[2], ymin = -Inf, ymax = +Inf, alpha = .2) + 
 annotate('text', x = xrng[1] + (xrng[2] - xrng[1])/2, y = yrng[2]-1, label = 'grazed',  colour = 'darkgrey')
-gg = ggplot(data, aes(Day, GooseGrazing)) + geom_line() + scale
-d = ggplot(data, aes(Day, Digestability)) + geom_line() + scale
-db = ggplot(data, aes(Day, DeadBiomass)) + geom_line() + scale
-gb = ggplot(data, aes(Day, GreenBiomass)) + geom_line() + scale
-lat = ggplot(data, aes(Day, LATotal)) + geom_line() + scale
-lag = ggplot(data, aes(Day, LAGreen)) + geom_line() + scale
-w = ggplot(data, aes(Day, WeedBiomass)) + geom_line() + scale
-b = ggplot(data, aes(Day, Biomass)) + geom_line() + scale
+gg = ggplot(data, aes(Day, GooseGrazing)) + geom_line() + scale + theme
+d = ggplot(data, aes(Day, Digestability)) + geom_line() + scale + theme
+db = ggplot(data, aes(Day, DeadBiomass)) + geom_line() + scale + theme
+gb = ggplot(data, aes(Day, GreenBiomass)) + geom_line() + scale + theme
+lat = ggplot(data, aes(Day, LATotal)) + geom_line() + scale + theme
+lag = ggplot(data, aes(Day, LAGreen)) + geom_line() + scale + theme
+w = ggplot(data, aes(Day, WeedBiomass)) + geom_line() + scale + theme
+b = ggplot(data, aes(Day, Biomass)) + geom_line() + scale + theme
 
 grid.arrange(h, gg, d, db, gb, lat, lag, w, b, nrow = 3, ncol = 3)
 }
