b77988e834cf083dda13ec04e585e71306bf4781
LDalby <lars@bios.au.dk>
2015-03-12 21:06:42 +0100

    Add PlotGooseHabitatUse function
    Currently just a wrapper around ggplot histogram (more or less)

---
+++ R/PlotGooseHabitatUse.r
@@ -0,0 +1,33 @@
+#' Plot goose habitat use
+#'
+#' Plot the frequency of use of particular habitat types
+#' 
+#' @param data data.frame or data.table The ALMaSS data file GooseFieldForage.txt
+#' @param species character The species to plot. Either 'Barnacle', 'Pinkfoot' or 'Greylag'
+#' @return A nice plot
+#' @export
+
+PlotGooseHabitatUse = function(data, species = NULL){
+	if(is.null(species)) 
+	{
+		stop('Please provide a species name\nEither Barnacle, Pinkfoot or Greylag\n')
+	}
+	species = tolower(species)
+	if(species == 'barnacle') 
+	{
+		data = data[Barnacle > 0,]
+	}
+	if(species == 'pinkfoot') 
+	{
+		data = data[Pinkfoot > 0,]
+	}
+	if(species == 'greylag') 
+	{
+		data = data[Greylag > 0,]
+	}
+habitatuse =  ggplot(data[species > 0,], aes(x=VegType))
+habitatuse = habitatuse + geom_bar() + facet_wrap(~ Month) + theme_bw()
+return(habitatuse)
+}
+
+
