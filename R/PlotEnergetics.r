853900ba303bd2f0fe9540e34f993288eab68b98
LDalby <lars@bios.au.dk>
2015-03-25 10:06:25 +0100

    fix bug when scales = NULL

--- R/PlotEnergetics.r
+++ R/PlotEnergetics.r
@@ -9,23 +9,27 @@
 #' @param type character Currently only accepts "mean".
 #' @param species character What species to plot? Defaults to "all"
 #' @param package character Base- or ggplot2-type ? Defaults to "ggplot2"
+#' @param scales character The argument to be used in facet_wrap if the ggplot2
+#' package is used. Either free_x or free_y.
 #' @return A nice plot
 #' @export
 PlotEnergetics = function(data, species = 'all', package = 'ggplot2', scales = NULL)
 {
  if(!is.data.table(data))
  {
-   cat('You appear to have loaded your results file using read.table().\n')
-   cat('please use the fread function in the package data.table')
-   return()
- }
- if(!species %in% c('all', 'PFF', 'PFNB', 'BGF', 'BGNB', 'GLF', 'GLNB'))
- {
-  cat('\n')
-  cat('Something is not right with the species argument.\n')
-  cat('Please provide one of these: \n')
-  cat('all', 'PFF', 'PFNB', 'BGF', 'BGNB', 'GLF', 'GLNB\n')
-  return()
+  stop(cat('You appear to have loaded your results file using read.table().\n
+    please use the fread function in the package data.table\n'))
+}
+if(!species %in% c('all', 'PFF', 'PFNB', 'BGF', 'BGNB', 'GLF', 'GLNB'))
+{
+  stop(cat('\n Something is wrong with the species argument.\n
+    Please provide one of these: \n all', 'PFF', 'PFNB', 'BGF',
+    'BGNB', 'GLF', 'GLNB\n'))
+}
+if(!is.null(scales)) {
+  if(!scales %in% c('free_x', 'free_y.')) {
+    stop(cat('scales only implemented for free_x & free_y.\n'))
+  }
 }
 
 setkey(data, 'Goose Type')
@@ -74,17 +78,26 @@ if(species == 'all' & package == 'base')
   par(mfrow = c(1,1), mar = c(5, 4, 4, 2) + 0.1, mgp = c(3, 1, 0))
 }
 
-if(species == 'all' & package == 'ggplot2' & scales == 'free_x') 
+if(species == 'all' & package == 'ggplot2') 
 {
-  ggplot(data[c('BGF', 'PFF', 'GLNB')], aes(SimDate, Weight)) + geom_line(aes(group = geesePA)) +
-   facet_wrap( ~ GooseType, scales = 'free_x')
+  if(!is.null(scales)) {
+    if(scales == 'free_x') {
+      ggplot(data[c('BGF', 'PFF', 'GLNB')], aes(SimDate, Weight)) + geom_line(aes(group = geesePA)) +
+      facet_wrap( ~ GooseType, scales = 'free_x')
+    }
+    if(scales == 'free_y') {
+      ggplot(data[c('BGF', 'PFF', 'GLNB')], aes(SimDate, Weight)) + geom_line(aes(group = geesePA)) +
+      facet_wrap( ~ GooseType, scales = 'free_y')
+    }
+  }
+
+  if(is.null(scales)) 
+  {
+    ggplot(data[c('BGF', 'PFF', 'GLNB')], aes(SimDate, Weight)) + geom_line(aes(group = geesePA)) + 
+    facet_wrap( ~ GooseType)
+  }
 }
 
-if(species == 'all' & package == 'ggplot2' & is.null(scales))
-{
-  ggplot(data[c('BGF', 'PFF', 'GLNB')], aes(SimDate, Weight)) + geom_line(aes(group = geesePA)) + 
-  facet_wrap( ~ GooseType)
-}
 
 if(species != 'all' & package == 'base') 
 {
