4d6a69451e21277ddb91a7e4e6592e9b511e3aa2
LDalby <lars@bios.au.dk>
2015-05-06 14:23:24 +0200

    Add classification functions and update docu

---
+++ R/ClassifyHabitatUse.r
@@ -0,0 +1,37 @@
+#' Classify animal habitat use
+#'
+#' Classify animal habitat use from what ALMaSS uses into categories
+#' matching the ones that are scored in the field
+#' 
+#' @param data data.table A data.table with the habitat use from ALMaSS
+#' @param species character String giving the species (or animal model)
+#' @return A data.table with the classifed habitat use.
+#' @export
+ClassifyHabitatUse = function(data, species = 'Goose')
+{
+	if(tolower(species) != 'goose') 
+	{
+		stop('Currently only implemented for geese')
+	}
+	if(tolower(species) == 'goose')
+	{
+	# Remove extra white spaces and make combined variable:
+		data[, VegTypeChr:=str_trim(VegTypeChr, side = 'right')]
+		data[, PreviousCrop:=str_trim(PreviousCrop, side = 'right')]
+		data[, VegTypeCombo:=paste(VegTypeChr, VegPhase, sep = '-')]
+
+	# Classify habitat use
+		data[Pinkfoot > 0, HabitatUsePF:=sapply(VegTypeCombo, FUN = ClassifyVegType)]
+		data[Barnacle > 0, HabitatUseBN:=sapply(VegTypeCombo, FUN = ClassifyVegType)]
+		data[Greylag > 0, HabitatUseGL:=sapply(VegTypeCombo, FUN = ClassifyVegType)]
+
+		data[Pinkfoot > 0 & VegPhase == 2 & VegHeight < 30, HabitatUsePF:=sapply(PreviousCrop, FUN = ClassifyPrevCrop)]
+		data[Barnacle > 0 & VegPhase == 2 & VegHeight < 30, HabitatUseBN:=sapply(PreviousCrop, FUN = ClassifyPrevCrop)]
+		data[Greylag > 0 & VegPhase == 2 & VegHeight < 30, HabitatUseGL:=sapply(PreviousCrop, FUN = ClassifyPrevCrop)]
+	# The three species has giving-up densities:
+		data[Grain > 98 & HabitatUseBN != 'Maize', HabitatUseBN:='Stubble']
+		data[Grain > 241 & HabitatUseGL != 'Maize', HabitatUseGL:='Stubble']
+		data[Grain > 134 & HabitatUsePF != 'Maize', HabitatUsePF:='Stubble']
+	}
+	return(data)
+}
---
+++ R/ClassifyPreviousCrop.r
@@ -0,0 +1,29 @@
+#' Classify Veg type based on the previous crop with respect to geese forage
+#'
+#' Classify the veg types from the detailed ones ALMaSS uses into categories
+#' matching the ones that are scored in the field.
+#' 
+#' This function handles the special cases where crop type changes before the 
+#' actual farm management takes place in the simulation.
+#' The function is designed to be used in combination with 'sapply' 
+#' 
+#' @param PreviousCrop character A string with the name of the previous crop.
+#' @return The vegtype matching the one scored in the field
+#' @export
+ClassifyPrevCrop = function(PreviousCrop)
+{
+	switch(EXPR = PreviousCrop,
+	# Grass               
+		'CloverGrassGrazed1' = 'Grass',
+		'OCloverGrassGrazed1' = 'Grass',
+		'CloverGrassGrazed2' = 'Grass',
+		'OCloverGrassGrazed2' = 'Grass',
+		'OBarleyPeaCloverGrass' = 'Grass',
+		'OSBarleySilage' = 'Grass',	
+		'None' = 'Grass',  # If no preceeding crop, it is a permanent grassy type
+	# Stubble
+		'SprBarleyCloverGrass' = 'Stubble',
+		'SpringBarleyCloverGrass' = 'Stubble',
+	# Default
+		'SomeFunkyPreceedingCrop')
+}
