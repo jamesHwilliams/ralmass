c0366f1cf847cbb996141d3f266c4a7cba692f82
LDalby <lars@bios.au.dk>
2015-06-23 15:23:52 +0200

    Update to double variable scenario generation

--- R/GenerateParams.r
+++ R/GenerateParams.r
@@ -7,14 +7,46 @@
 #' @param start numeric Starting value.
 #' @param end numeric End value.
 #' @param N Number of required values.
-#' @return data.frame Two column data.frame. First column the config variable
-#' and second column being the values.
+#' @param Config The character string with the config variable (including (int))
+#' @param start2 numeric Starting value for the (optional) second variable
+#' @param end2 numeric End value for the (optional) second variable
+#' @param N2 Number of required values for the (optional) second variable
+#' @param Config2 The character string with the second config variable (including (int))
+#' @return data.frame One column each value a text string with the config and 
+#' and its value
 #' @export
 
-GenerateParams = function(start, end, N, Config)
+GenerateParams = function(start, end, N, Config,
+ start2 = NULL, end2 = NULL, N2 = NULL, Config2 = NULL,
+ write = FALSE)
 {
-	val = seq(start, end, length.out = N)
-	data.frame('Config' = rep(Config, N), 'Value' = val)
+	if(is.null(start2) | is.null(end2) | is.null(N2) | is.null(Config2))
+	{
+		val = seq(start, end, length.out = N)
+		df = data.frame('Config' = rep(Config, N), 'Value' = val)
+	}
+	if(!is.null(start2) | !is.null(end2) | !is.null(N2) | !is.null(Config2))
+	{
+		val = seq(start, end, length.out = N)
+		val2 = seq(start2, end2, length.out = N2)
+		a = rep(val, each = length(val2))
+		b = rep(val2, length(val))
+		end = rep(NA, length(a))
+		uneven = seq(1, length(a)*2, by = 2)
+		even = seq(2, length(a)*2, by = 2)
+		end[uneven] = a
+		end[even] = b
+		confignames = rep(c(Config, Config2), length(a))
+		df = data.frame('Params' = paste(confignames, end, sep = ' = '))
+	}
+	if(write)
+	{
+		write.table(df, file = 'ParameterValues.txt', sep = '\t', quote = FALSE,
+			row.names = FALSE, col.names = FALSE)
+		cat('Printed following to ParameterValues.txt:\n')
+	}
+	return(df)
 }
 
-GenerateParams(0, 100, 11, 'asd')
+setwd('c:/Users/lada/Desktop/')
+GenerateParams(0, 10, 5, 'Blaa (int)', 1, 3, 3, 'Blaa (double)', write = TRUE)
