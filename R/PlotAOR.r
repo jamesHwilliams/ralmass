6c44bf0599655894c355e4637924de5e6194e808
LDalby <lars.dalby>
2015-06-29 22:19:04 +0200

    continued devel

--- R/PlotAOR.r
+++ R/PlotAOR.r
@@ -15,24 +15,30 @@
 #' @export
 # AOR hacking:
 
-library(RColorBrewer)
 df = data.frame('x' = rnorm(5, 0, .5), 'y' = rnorm(5, 0, .5), 'scenario' = letters[1:5])
-scenarios = unique(as.character(df$scenario))
-col = brewer.pal(length(scenarios), 'Set2')
+PlotAOR = function(data, x = NULL, y = NULL, scenarios = NULL)
+{
+	
+scenarios = unique(as.character(df[,scenarios]))
+col = RColorBrewer::brewer.pal(length(scenarios), 'Set2')
 
-	plot(-1:1,-1:1, type = 'n', las = 1, xlab = 'Abundance', ylab = 'Occupancy')
-	abline(h = 0)
-	abline(v = 0)
+plot(-1:1,-1:1, type = 'n', las = 1, xlab = 'Abundance', ylab = 'Occupancy')
+abline(h = 0)
+abline(v = 0)
 
 for (i in 1:nrow(df)) {
 	with(df[which(df$scenario == scenarios[i]),], {
 		lines(rbind(c(0,0), c(x, y)), col = col[i], lwd = 2)
-		# segments(0,0, -.5, -.5, col = 'green', lwd = 3)
 		# segments(-.5,-.45, -.5, -.55, col = col[i], lwd = 1.5)
 		# segments(-.45,-.5, -.55, -.5, col = col[i], lwd = 1.5)
-		points(x,y, pch = 21, col = 'darkgrey', cex = 1.5, bg = col[i])
+		points(x,y, pch = 21, col = 'darkgrey', cex = 1.4, bg = col[i])
 	})
 }
 
 legend(-1, 1.3, legend = scenarios, bty = 'n', horiz = TRUE, 
-	xpd = NA, pch = 21, cex = 1.5, pt.bg = col, col = 'darkgrey')
+	xpd = NA, pch = 21, cex = 1.4, pt.bg = col, col = 'darkgrey')
+}
+
+
+library(ggplot2)
+ggplot(df, aes(x,y, fill = scenario)) + geom_point()
