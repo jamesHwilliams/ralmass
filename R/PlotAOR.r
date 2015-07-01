b20adeaa77ced6edb1d4a987514c4424733e012f
LDalby <lars@bios.au.dk>
2015-07-01 09:51:24 +0200

    Roxygen annotation and add missing ref to package

--- R/PlotAOR.r
+++ R/PlotAOR.r
@@ -10,8 +10,9 @@
 #' @param scenarios character The name of the column indicating which scenario
 #' a given set of coordinates belong to.
 #' @return An AOR plot
-#' @references Høye, T. T., et al. (2012). "Interpreting outputs of agent-based models
-#'  using abundance-occupancy relationships." Ecological Indicators 20: 221-227.
+#' @references Høye, T. T., Skov, F. & Topping, C.J. (2012). Interpreting
+#' outputs of agent-based models using abundance-occupancy relationships. 
+#' Ecological Indicators 20: 221-227.
 #' @export
 #' @examples
 #' df = data.frame('x' = rnorm(8, 0, .5), 'y' = rnorm(8, 0, .5), 'scenario' = letters[1:8])
@@ -19,16 +20,18 @@
 PlotAOR = function(data, x = NULL, y = NULL, scenarios = NULL)
 {
 # Append the origo point to the data:
-  dataorigo = data.frame(x = rep(0, nrow(data)), y = rep(0, nrow(data)), 'scenario' = unique(data[,scenarios]))
+  dataorigo = data.frame(x = rep(0, nrow(data)), y = rep(0, nrow(data)),
+   'scenario' = unique(data[,scenarios]))
   dataorigo = rbind(data, dataorigo)
 # Setup the plot
-  p = ggplot2::ggplot(dataorigo, aes_string(x,y)) +
+  p = ggplot2::ggplot(dataorigo, ggplot2::aes_string(x,y)) +
   ggplot2::geom_vline(xintercept = 0) +
   ggplot2::geom_hline(yintercept = 0) +
-  ggplot2::geom_line(aes(color = scenario)) +
-  ggplot2::geom_point(data = data, aes_string(x,y, color = scenario), size = 3) +
+  ggplot2::geom_line(ggplot2::aes(color = scenario)) +
+  ggplot2::geom_point(data = data, ggplot2::aes_string(x,y, color = scenarios), size = 3) +
   ggplot2::ggtitle('AOR') +
-  ggplot2::labs(y = expression(paste(Delta, 'Abundance')), x = expression(paste(Delta, 'Occupancy'))) +
+  ggplot2::labs(y = expression(paste(Delta, 'Abundance')), 
+    x = expression(paste(Delta, 'Occupancy'))) +
   ggplot2::theme_bw() +
   ggplot2::ylim(-1,1) +
   ggplot2::xlim(-1,1)
