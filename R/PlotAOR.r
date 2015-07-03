5d84ccedc1fa37665344943c6300af63edfdf46f
LDalby <lars@bios.au.dk>
2015-07-03 15:55:24 +0200

    Rename tri to triangle and update documentation

--- R/PlotAOR.r
+++ R/PlotAOR.r
@@ -11,8 +11,12 @@
 #' a given set of coordinates belong to.
 #' @param title character The title of the plot (optional).
 #' @param fixed logical If TRUE (the default) plot has xlim and ylim -1,1.
-#' @param tri logical Should the area of decrease on both axes be highlighted?
+#' @param triangle logical Should the area of decrease on both axes be highlighted?
 #' default is FALSE.
+#' @param trigger numeric The trigger value for accepted decrease in the
+#' summed-AOR indexs score. Mostly used when assessing impact of pesticides.
+#' Any points falling beyond the area highlighted by the shaded triangle are an
+#' unacceptable level of impact.
 #' @return An AOR plot
 #' @references Hoye, T. T., Skov, F. & Topping, C.J. (2012). Interpreting
 #' outputs of agent-based models using abundance-occupancy relationships. 
@@ -21,16 +25,17 @@
 #' @examples
 #' df = data.frame('x' = rnorm(8, 0, .5), 'y' = rnorm(8, 0, .5), 'scenario' = letters[1:8])
 #' PlotAOR(df, x = 'x', y = 'y', scenarios = 'scenario')
-PlotAOR = function(data, x = NULL, y = NULL, scenarios = NULL, fixed = TRUE, title = NULL, tri = FALSE)
+PlotAOR = function(data, x = NULL, y = NULL, scenarios = NULL, fixed = TRUE,
+ title = NULL, triangle = FALSE, trigger = 0)
 {
 # Append the origo point to the data:
-  if(is.data.table(data))
+  if(data.table::is.data.table(data))
   {
     dataorigo = data.frame(x = rep(0, nrow(data)), y = rep(0, nrow(data)),
      'scenario' = unique(data[,scenarios, with = FALSE]))
     setnames(dataorigo, c(x,y,scenarios))
   }
-  if(!is.data.table(data))
+  if(!data.table::is.data.table(data))
   {
     dataorigo = data.frame(x = rep(0, nrow(data)), y = rep(0, nrow(data)),
      'scenario' = unique(data[,scenarios]))
@@ -39,17 +44,18 @@ PlotAOR = function(data, x = NULL, y = NULL, scenarios = NULL, fixed = TRUE, tit
   dataorigo = rbind(data, dataorigo)
 # Setup the plot
   p = ggplot2::ggplot(dataorigo, ggplot2::aes_string(x,y))
-  if(tri)
+  if(triangle)
   {
-    tri = data.frame(x = c(-1,-1,1), y = c(1,-1,-1))
-    p = p + geom_polygon(data = tri, aes(x=x, y=y),alpha = 0.05)
+    trigger = abs(trigger)
+    triangle = data.frame(x = c(-1,-1,1-trigger), y = c(1-trigger,-1,-1))
+    p = p + ggplot2::geom_polygon(data = triangle, ggplot2::aes(x=x, y=y), alpha = 0.05)
   }
   p = p + ggplot2::geom_vline(xintercept = 0) +
   ggplot2::geom_hline(yintercept = 0) +
   ggplot2::geom_line(ggplot2::aes_string(color = scenarios)) +
   ggplot2::geom_point(data = data, ggplot2::aes_string(x,y, color = scenarios), size = 3) +
-  ggplot2::labs(y = expression(paste(Delta, 'Abundance')), 
-    x = expression(paste(Delta, 'Occupancy'))) +
+  ggplot2::labs(y = expression(paste(Delta, ' Abundance')), 
+    x = expression(paste(Delta, ' Occupancy'))) +
   ggplot2::theme_bw()
   if(!is.null(title))
   {
