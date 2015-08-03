dd21b364710ba28d613cb14b3285db93ec58420e
LDalby <lars@bios.au.dk>
2015-08-03 09:49:13 +0200

    Add argument to get only a point plot

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -6,29 +6,36 @@
 #' @return A nice plot
 #' @export
 
-PlotGoosePopulation = function(data){
-data[,SimDate:=as.Date(DayInYear, origin = as.Date(paste(Year+1989,"-01-01", sep = '')))]
-data[,c('Day', 'Month', 'Year', 'Time', 'DayInYear'):=NULL]
+PlotGoosePopulation = function(data, type = 'point'){
+	data[,SimDate:=as.Date(DayInYear, origin = as.Date(paste(Year+1989,"-01-01", sep = '')))]
+	data[,c('Day', 'Month', 'Year', 'Time', 'DayInYear'):=NULL]
 
-melted = reshape2::melt(data, id.vars = c('SimDate'),
- variable.name = 'GooseType', value.name = 'Numbers')
-melted = melted[Numbers != 0,]
-ggplot2::theme_update(
-	panel.background = ggplot2::element_blank(),
-	axis.text.x = ggplot2::element_text(colour = "black", face = "plain", size = 10),
-	axis.text.y = ggplot2::element_text(colour = "black", face = "plain", size = 10),
-	axis.title.x = ggplot2::element_text(colour = "black", face = "plain", size = 12),
-	axis.title.y = ggplot2::element_text(colour = "black", face = "plain", angle = 90, size = 12),
-	panel.grid.minor = ggplot2::element_blank(),
-	panel.grid.major = ggplot2::element_blank(),
-	panel.background = ggplot2::element_blank(),
-	axis.line = ggplot2::element_line(size = 0.5),
-	axis.ticks = ggplot2::element_line(colour = "black", size = 0.5)
-)
+	melted = reshape2::melt(data, id.vars = c('SimDate'),
+		variable.name = 'GooseType', value.name = 'Numbers')
+	melted = melted[Numbers != 0,]
+	ggplot2::theme_update(
+		panel.background = ggplot2::element_blank(),
+		axis.text.x = ggplot2::element_text(colour = "black", face = "plain", size = 10),
+		axis.text.y = ggplot2::element_text(colour = "black", face = "plain", size = 10),
+		axis.title.x = ggplot2::element_text(colour = "black", face = "plain", size = 12),
+		axis.title.y = ggplot2::element_text(colour = "black", face = "plain", angle = 90, size = 12),
+		panel.grid.minor = ggplot2::element_blank(),
+		panel.grid.major = ggplot2::element_blank(),
+		panel.background = ggplot2::element_blank(),
+		axis.line = ggplot2::element_line(size = 0.5),
+		axis.ticks = ggplot2::element_line(colour = "black", size = 0.5)
+		)
 
-pop = ggplot2::ggplot(melted, ggplot2::aes(SimDate, Numbers, group = GooseType)) +
-	ggplot2::geom_line(aes(colour = GooseType)) + ggplot2::geom_point(aes(colour = GooseType))  + ggplot2::ylab('Numbers')
-return(pop)
+	pop = ggplot2::ggplot(melted, ggplot2::aes(SimDate, Numbers, group = GooseType))
+	if(tolower(type) == 'point') 
+	{
+		pop = pop + ggplot2::geom_point(aes(colour = GooseType)) + ggplot2::ylab('Numbers')
+	}
+	if(tolower(type) != 'point') 
+	{
+		pop = pop + ggplot2::geom_line(aes(colour = GooseType)) + ggplot2::geom_point(aes(colour = GooseType))  + ggplot2::ylab('Numbers')
+	}
+	return(pop)
 }
 
 
