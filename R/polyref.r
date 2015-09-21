12590d3161efa3fd4ffd68440a6a9af1d6367838
LDalby <lars@bios.au.dk>
2015-09-21 12:04:56 +0200

    Move itemized documentation

--- R/polyref.r
+++ R/polyref.r
@@ -2,14 +2,15 @@
 #'
 #' The table contains 1000 rows and 4 columns
 #'
-#' @docType data
-#' @usage polyref
-#' \itemize{
+#'\itemize{
 #'   \item OBJECTID. This is the polygon ID.
 #'   \item VALUE. So is this. A duplicate of OBJECTID.
 #'   \item COUNT. The number of cells within the polygon.
 #'   \item LINK. The type of landscape element.
 #'  }
+#' 
+#' @docType data
+#' @usage polyref
 #' @format A data frame with 1000 rows and 4 variables
 #' @name polyref
 NULL
