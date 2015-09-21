83f9d14fd85b0b5f37568841feed61eaae4eae01
LDalby <lars@bios.au.dk>
2015-09-21 12:51:09 +0200

    Further documentation updates

--- R/WritePolyref.r
+++ R/WritePolyref.r
@@ -3,7 +3,10 @@
 #' Write a polyref file to a text file ready for loading in ALMaSS.
 #'
 #' @param Table data.frame or data.table An attribute table
-#' @param PathToFile The path to the where the file is written (including the name and extension of the file, usually .txt)
+#' @param PathToFile character The path to the where the file is 
+#' written (including the name and extension of the file, usually .txt)
+#' @param Headers logical Should the output file include headers?
+#' @param Type character Write poly- or farm-ref file?
 #' @return A tab separated text file formatted according to the requirements for a polygon reference file for ALMaSS.
 #' @export
 
