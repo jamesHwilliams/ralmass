a172bbdf075c1f8d932419229bf40042fe0bd3c8
LDalby <lars@bios.au.dk>
2015-10-14 11:49:59 +0200

    Add func to check for illegal densities of hunters

---
+++ R/CheckDensity.r
@@ -0,0 +1,27 @@
+#' Check densities of hunters per farm
+#'
+#' Do a check of the densities of hunters per farm. 
+#' This is just ensuring that the density criteria is not broken.
+#' 
+#' 
+#' @param data data.table The file HuntingLocationsFarm.txt
+#' @param MaxDensity numeric The density paramter setting
+#' @return logical TRUE if all densities legal, FALSE otherwise
+#' @export
+CheckDensity = function(data = NULL, MaxDensity = NULL) 
+{
+	if(is.null(data) | is.null(MaxDensity))
+	{
+		stop('Input parameter missing')
+	}
+	AnyRows = nrow(data[Numbers > MaxDensity]) != 0
+	if(AnyRows != 0)
+	{
+		return(FALSE)
+	}
+	if(AnyRows == 0)
+	{
+		return(TRUE)
+	}
+}
+
