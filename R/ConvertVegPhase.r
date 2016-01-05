4dc60a02aa043a2c26d3e99b5a0ef0ee930ea216
LDalby <lars@bios.au.dk>
2016-01-05 10:01:33 +0100

    Add simple function to translate vegphase numbers

---
+++ R/ConvertVegPhase.r
@@ -0,0 +1,18 @@
+#' Convert numeric vegphase to its character name
+#'
+#' Spell out the full name of the vegphase from the numeric code
+#' 
+#' @param VegPhase integer A vegphase number
+#' @return The vegphase name
+#' @export
+ConvertVegPhase = function(VegPhase) {
+	# Since the numeric version of switch starts at 1, I convert to character.
+	x = as.character(VegPhase)
+	switch(x,
+		'0' = 'janfirst',
+		'1' = 'sow',
+		'2' = 'marchfirst',
+		'3' = 'harvest1',
+		'4' = 'harvest2',
+		)
+}
