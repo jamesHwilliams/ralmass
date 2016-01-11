8cd210e8295268ab2ee2ea3122784886cd7a554b
LDalby <lars@bios.au.dk>
2016-01-11 16:12:49 +0100

    Fix indentation

--- R/PlotEnergetics.r
+++ R/PlotEnergetics.r
@@ -13,54 +13,54 @@
 #' @return A nice plot
 #' @export
 PlotEnergetics = function(SimData, FieldData, Sample = NULL) {
- if(!is.data.table(SimData))
- {
-  stop(cat('You appear to have loaded your results file using read.table().\n
-    please use the fread function in the package data.table\n'))
-}
-if('Goose Type' %in% names(SimData))
-{
-  data.table::setnames(SimData, old = 'Goose Type', new = 'GooseType')
-}
+	if(!is.data.table(SimData))
+	{
+		stop(cat('You appear to have loaded your results file using read.table().\n
+			please use the fread function in the package data.table\n'))
+	}
+	if('Goose Type' %in% names(SimData))
+	{
+		data.table::setnames(SimData, old = 'Goose Type', new = 'GooseType')
+	}
 
-data.table::setkey(SimData, 'GooseType')
-SimData = SimData[GooseType == 'PFF']
+	data.table::setkey(SimData, 'GooseType')
+	SimData = SimData[GooseType == 'PFF']
 
-ys = unique(SimData[,Year])
-ys = ys+2011  # Quick fix to match dates from field data
-ysorigins = as.Date(paste0(ys, '-01-01'))
+	ys = unique(SimData[,Year])
+	ys = ys+2011  # Quick fix to match dates from field data
+	ysorigins = as.Date(paste0(ys, '-01-01'))
 # setkey(data, 'Goose Type')
-for (i in seq_along(ys)) {
-  SimData[Year == i, Date:=as.Date(Day, origin = ysorigins[i])]
-}
+	for (i in seq_along(ys)) {
+		SimData[Year == i, Date:=as.Date(Day, origin = ysorigins[i])]
+	}
 
-SimData[, Season:=c(0, cumsum(diff(Month) > 1))]  # okay
-SimData[, Type:='Sim']
+	SimData[, Season:=c(0, cumsum(diff(Month) > 1))]  # okay
+	SimData[, Type:='Sim']
 
-if(!is.null(Sample))
-{
-  nrows = nrow(SimData)
-  rows = sample(1:nrows, Sample*nrows)
-  SimData = SimData[rows,]
-}
+	if(!is.null(Sample))
+	{
+		nrows = nrow(SimData)
+		rows = sample(1:nrows, Sample*nrows)
+		SimData = SimData[rows,]
+	}
 
-FieldData = FieldData[Date > lubridate::dmy('29-08-2012') &
-Date < lubridate::dmy('31-12-2013')]
-FieldData = FieldData[lubridate::month(FieldData$Date) < 5 | lubridate::month(FieldData$Date) > 9,]
-FieldData[, Season:=c(0, cumsum(diff(Date)/ddays(1) > 100))]
-FieldData[,Season:=Season+100]
+	FieldData = FieldData[Date > lubridate::dmy('29-08-2012') &
+	Date < lubridate::dmy('31-12-2013')]
+	FieldData = FieldData[lubridate::month(FieldData$Date) < 5 | lubridate::month(FieldData$Date) > 9,]
+	FieldData[, Season:=c(0, cumsum(diff(Date)/ddays(1) > 100))]
+	FieldData[,Season:=Season+100]
 
-SimData[, Date:=lubridate::ymd(Date)]
-SimData = SimData[GooseType == 'PFF',.(Date, Weight, Type, Season)]
-FieldData[, Type:='Field']
-temp = rbind(SimData, FieldData[, .(Date, Weight, Type, Season)])
-setkey(temp, 'Date')
-temp[, Type:=as.factor(Type)]
+	SimData[, Date:=lubridate::ymd(Date)]
+	SimData = SimData[GooseType == 'PFF',.(Date, Weight, Type, Season)]
+	FieldData[, Type:='Field']
+	temp = rbind(SimData, FieldData[, .(Date, Weight, Type, Season)])
+	setkey(temp, 'Date')
+	temp[, Type:=as.factor(Type)]
 
-p = ggplot(temp, aes(Date, Weight, color = Type)) +
-geom_point(alpha = 1/50) + 
-geom_smooth(aes(group = Season)) +
-theme_bw() + 
-theme(axis.text=element_text(size=8))
-return(p)
+	p = ggplot(temp, aes(Date, Weight, color = Type)) +
+	geom_point(alpha = 1/50) + 
+	geom_smooth(aes(group = Season)) +
+	theme_bw() + 
+	theme(axis.text=element_text(size=8))
+	return(p)
 }
