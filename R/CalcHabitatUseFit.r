ef1da839c8b747c89ab0e7ff4661c8365ef27676
LDalby <lars@bios.au.dk>
2016-01-31 15:48:00 +0100

    Add CalcHabitatUseFit

---
+++ R/CalcHabitatUseFit.r
@@ -0,0 +1,60 @@
+#' Calculate the fit of habitat use frequencies
+#'
+#' Calculate the fit between habitat use scored in the field 
+#' and habitat use based on the simulations
+#'
+#' @param FieldData data.table The table with the field data
+#' @param SimData data.table The field forage output file from ALMaSS
+#' @return data.table The fit per species and month
+#' @export
+CalcHabUseFit = function(FieldData, SimData) {
+# Field data:
+	h = unique(FieldData[, HabitatUse])
+	sp = unique(FieldData[, Species])
+	mths = unique(FieldData[, Month])
+	fieldcombs = expand.grid(N = 0, HabitatUse = h, Month = mths, Species =  sp,
+		stringsAsFactors = FALSE) %>% data.table::as.data.table
+	FieldData = rbind(FieldData, fieldcombs)
+	cols = c('Species', 'HabitatUse', 'Month')
+	FieldData = FieldData[, lapply(.SD, sum, na.rm = TRUE), by = cols]
+	FieldData[, NMTotal:=sum(N), by=.(Month, Species)]
+	FieldData[, PropField:=N/NMTotal]
+	FieldData[, c('N', 'NMTotal'):=NULL]
+	data.table::setkeyv(FieldData, cols)
+# field data end
+	
+# Sim data:
+	SimData = SimData[Month %in% mths,]
+# Pinkfoot
+	hbpf = SimData[, .(Pinkfoot, HabitatUsePF, Month)]
+	hbpf[, Species:='Pinkfoot']
+	newnames = c('HabitatUse', 'N')
+	data.table::setnames(hbpf, old = c('HabitatUsePF', 'Pinkfoot'), new = newnames)
+# Greylag
+	hbgl = SimData[, .(Greylag, HabitatUseGL, Month)]
+	hbgl[, Species:='Greylag']
+	data.table::setnames(hbgl, old = c('HabitatUseGL', 'Greylag'), new = newnames)
+# Barnacle
+	hbbn = SimData[, .(Barnacle, HabitatUseBN, Month)]
+	hbbn[, Species:='Barnacle']
+	data.table::setnames(hbbn, old = c('HabitatUseBN', 'Barnacle'), new = newnames)
+# Full data
+	hb = rbind(hbpf, hbbn, hbgl)
+	hb = hb[complete.cases(hb),]
+
+	h = unique(c(hb[,HabitatUse], h))
+	combs = expand.grid(N = 0, HabitatUse = h, Month = mths,
+		Species = sp, stringsAsFactors = FALSE) %>% data.table::as.data.table
+
+	temp = rbind(hb, combs)
+	temp[,NMTotal:=sum(N), by=.(Month, Species)]
+	temp[, PropSim:=N/NMTotal]
+	temp = temp[, lapply(.SD, sum, na.rm = TRUE), by = cols]
+	temp = temp[, .(HabitatUse, Month, Species, PropSim)][is.na(PropSim), PropSim:=0] %>% unique
+	data.table::setkeyv(temp, cols)
+
+	HabFit = data.table::merge(temp, habuse)
+	cols = c('Species', 'Month')
+	HabFit = HabFit[, Fit:=1-sum((PropSim-PropField)^2), by = cols]
+	HabFit = HabFit[, .(Species, Month, Fit)] %>% unique
+}
