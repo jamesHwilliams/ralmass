c995853ff2c593f8b38b45af1ea27be840641cf1
LDalby <lars@bios.au.dk>
2016-02-03 10:19:04 +0100

    Setkey on habfit and return

--- R/CalcHabitatUseFit.r
+++ R/CalcHabitatUseFit.r
@@ -67,4 +67,6 @@ CalcHabitatUseFit = function(FieldData, SimData) {
 	HabFit[Species == 'Barnacle' & !Month %in% hbbnMonths, Fit:=NA]
 	HabFit[Species == 'Greylag' & !Month %in% hbglMonths, Fit:=NA]
 	HabFit[Species == 'Pinkfoot' & !Month %in% hbpfMonths, Fit:=NA]
+	setkeyv(HabFit, cols)
+	return(HabFit)
 }
