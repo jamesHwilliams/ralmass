3c571c07ff52773f7741da35ecbc6d093efb34c8
LDalby <lars@bios.au.dk>
2016-04-07 14:19:17 +0200

    Don't use custom theme and reshape2

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -6,25 +6,14 @@
 #' @param type character The plot type. Either 'point' or 'line'.
 #' @return A nice plot
 #' @export
-
 PlotGoosePopulation = function(data, type = 'point'){
-	melted = reshape2::melt(data, id.vars = c('Day'),
+	melted = data.table::melt(data, id.vars = 'Day',
 		variable.name = 'GooseType', value.name = 'Numbers')
 	melted = melted[Numbers != 0,]
-	ggplot2::theme_update(
-		panel.background = ggplot2::element_blank(),
-		axis.text.x = ggplot2::element_text(colour = "black", face = "plain", size = 10),
-		axis.text.y = ggplot2::element_text(colour = "black", face = "plain", size = 10),
-		axis.title.x = ggplot2::element_text(colour = "black", face = "plain", size = 12),
-		axis.title.y = ggplot2::element_text(colour = "black", face = "plain", angle = 90, size = 12),
-		panel.grid.minor = ggplot2::element_blank(),
-		panel.grid.major = ggplot2::element_blank(),
-		panel.background = ggplot2::element_blank(),
-		axis.line = ggplot2::element_line(size = 0.5),
-		axis.ticks = ggplot2::element_line(colour = "black", size = 0.5)
-		)
+	
+	pop = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) + 
+	theme_bw()
 
-	pop = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType))
 	if(tolower(type) == 'point') 
 	{
 		pop = pop + ggplot2::geom_point(aes(colour = GooseType)) +
