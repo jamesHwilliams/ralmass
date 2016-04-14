f8bed483c90dd1d980248dfbc31652ae57151c22
LDalby <lars@bios.au.dk>
2016-04-14 08:39:52 +0200

    Add habuse column even if species missing

--- R/ClassifyHabitatUse.r
+++ R/ClassifyHabitatUse.r
@@ -18,106 +18,112 @@ ClassifyHabitatUse = function(data, species = NULL, timed = TRUE) {
 	{
 		stop('Currently only implemented for geese')
 	}
-	if(tolower(species) == 'goose' & !timed)
+	if(tolower(species) == 'goose')
 	{
+		data[,c('HabitatUsePF', 'HabitatUseGL', 'HabitatUseBN'):='foo']
+		if(!timed)
+		{
 	# Remove extra white spaces and make combined variable:
-		data[, LastSownVeg:=stringr::str_trim(LastSownVeg, side = 'right')]
-		data[, VegTypeCombo:=paste(LastSownVeg, VegPhase, sep = '-')]
-		data[Pinkfoot > 0, HabitatUsePF:=sapply(VegTypeCombo, ClassifyVegType)]
-		data[Greylag > 0, HabitatUseGL:=sapply(VegTypeCombo, ClassifyVegType)]
-		data[Barnacle > 0, HabitatUseBN:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[, LastSownVeg:=stringr::str_trim(LastSownVeg, side = 'right')]
+			data[, VegTypeCombo:=paste(LastSownVeg, VegPhase, sep = '-')]
+			data[Pinkfoot > 0, HabitatUsePF:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[Greylag > 0, HabitatUseGL:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[Barnacle > 0, HabitatUseBN:=sapply(VegTypeCombo, ClassifyVegType)]
 		# Fix for a rare bug:
-		data[Pinkfoot > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseBN:='Stubble']
+			data[Pinkfoot > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseBN:='Stubble']
 		# Fix natural grass types:
-		data[Pinkfoot > 0 & VegTypeChr == 'NaturalGrass', HabitatUsePF:='Grass']
-		data[Greylag > 0 & VegTypeChr == 'NaturalGrass', HabitatUseGL:='Grass']
-		data[Barnacle > 0 & VegTypeChr == 'NaturalGrass', HabitatUseBN:='Grass']
+			data[Pinkfoot > 0 & VegTypeChr == 'NaturalGrass', HabitatUsePF:='Grass']
+			data[Greylag > 0 & VegTypeChr == 'NaturalGrass', HabitatUseGL:='Grass']
+			data[Barnacle > 0 & VegTypeChr == 'NaturalGrass', HabitatUseBN:='Grass']
 		# Fix undersown spring barley - CGG1:
-		data[Pinkfoot > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
-		data[Pinkfoot > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+			data[Pinkfoot > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+			data[Pinkfoot > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
 		# SG1
-		data[Pinkfoot > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
-		data[Pinkfoot > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
+			data[Pinkfoot > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
+			data[Pinkfoot > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
 		# Spring barley that is not undersown
-		data[Pinkfoot > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[Greylag > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[Barnacle > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
-		return(data)
-	}
-	if(tolower(species) == 'goose' & timed)
-	{
-	# Remove extra white spaces and make combined variable:
-		data[, LastSownVeg:=stringr::str_trim(LastSownVeg, side = 'right')]
-		data[, VegTypeCombo:=paste(LastSownVeg, VegPhase, sep = '-')]
-		data[PinkfootTimed > 0, HabitatUsePF:=sapply(VegTypeCombo, ClassifyVegType)]
-		data[GreylagTimed > 0, HabitatUseGL:=sapply(VegTypeCombo, ClassifyVegType)]
-		data[BarnacleTimed > 0, HabitatUseBN:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[Pinkfoot > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[Greylag > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[Barnacle > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+		}
+		if(timed)
+		{
+		# Remove extra white spaces and make combined variable:
+			data[, LastSownVeg:=stringr::str_trim(LastSownVeg, side = 'right')]
+			data[, VegTypeCombo:=paste(LastSownVeg, VegPhase, sep = '-')]
+			data[PinkfootTimed > 0, HabitatUsePF:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[GreylagTimed > 0, HabitatUseGL:=sapply(VegTypeCombo, ClassifyVegType)]
+			data[BarnacleTimed > 0, HabitatUseBN:=sapply(VegTypeCombo, ClassifyVegType)]
 		# Fix for a rare bug:
-		data[PinkfootTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseBN:='Stubble']
+			data[PinkfootTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & VegTypeCombo == 'OFieldPeas-3', HabitatUseBN:='Stubble']
 		# Fix natural grass types:
-		data[PinkfootTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUsePF:='Grass']
-		data[GreylagTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUseGL:='Grass']
-		data[BarnacleTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUseBN:='Grass']
+			data[PinkfootTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUsePF:='Grass']
+			data[GreylagTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUseGL:='Grass']
+			data[BarnacleTimed > 0 & VegTypeChr == 'NaturalGrass', HabitatUseBN:='Grass']
 		# Fix undersown spring barley - CGG1:
-		data[PinkfootTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
-		data[PinkfootTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+			data[PinkfootTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+			data[PinkfootTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
 		# SG1
-		data[PinkfootTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
-		data[PinkfootTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
-			 LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
+			data[PinkfootTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & VegTypeChr == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
+			data[PinkfootTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & PreviousCrop == 'SprBarleyCloverGrass' &
+			LastSownVeg == 'SeedGrass1', HabitatUseBN:='Stubble']
 		# Spring barley that is not undersown
-		data[PinkfootTimed > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
-		data[GreylagTimed > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
-		data[BarnacleTimed > 0 & PreviousCrop == 'SpringBarley' &
-			 LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
-		return(data)
+			data[PinkfootTimed > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUsePF:='Stubble']
+			data[GreylagTimed > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseGL:='Stubble']
+			data[BarnacleTimed > 0 & PreviousCrop == 'SpringBarley' &
+			LastSownVeg == 'CloverGrassGrazed1', HabitatUseBN:='Stubble']
+		}
+	data[HabitatUseBN == 'foo', HabitatUseBN:=NA]
+	data[HabitatUsePF == 'foo', HabitatUsePF:=NA]
+	data[HabitatUseGL == 'foo', HabitatUseGL:=NA]
+	return(data)
 	}
 }
