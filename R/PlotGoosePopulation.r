e253f8822184987ac4be73be747882a65decda69
LDalby <lars@bios.au.dk>
2016-05-24 08:21:11 +0200

    Rem option to plot only points

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -2,30 +2,18 @@
 #'
 #' Plot the number of geese per species and type
 #' 
-#' @param data data.frame or data.table The ALMaSS data file GoosePopulationData.txt
-#' @param type character The plot type. Either 'point' or 'line'.
+#' @param data data.table The ALMaSS data file GoosePopulationData.txt
 #' @return A nice plot
 #' @export
-PlotGoosePopulation = function(data, type = 'point'){
+PlotGoosePopulation = function(data){
 	melted = data.table::melt(data, id.vars = 'Day',
 		variable.name = 'GooseType', value.name = 'Numbers')
 	melted = melted[Numbers != 0,]
 	
-	pop = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) + 
-	theme_bw()
-
-	if(tolower(type) == 'point') 
-	{
-		pop = pop + ggplot2::geom_point(aes(colour = GooseType)) +
-		ggplot2::ylab('Numbers')
-	}
-	if(tolower(type) != 'point') 
-	{
-		pop = pop + ggplot2::geom_line(aes(colour = GooseType)) + 
-			ggplot2::geom_point(aes(colour = GooseType)) + 
-			ggplot2::ylab('Numbers')
-	}
-	return(pop)
+	p = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) + 
+	theme_bw() + ggplot2::geom_line(aes(colour = GooseType)) + 
+		ggplot2::geom_point(aes(colour = GooseType)) + ggplot2::ylab('Numbers')
+	return(p)
 }
 
 
