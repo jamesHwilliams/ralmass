04fa26e77ecf4bfc02149d3c27743c64d9ddfaf2
Lars Dalby <lars.dalby@gmail.com>
2016-06-21 11:28:09 +0200

    Enable option to plot dates

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -3,13 +3,16 @@
 #' Plot the number of geese per species and type
 #' 
 #' @param data data.table The ALMaSS data file GoosePopulationData.txt
+#' @param dates logical Should we plot day numbers as dates?
 #' @return A nice plot
 #' @export
 PlotGoosePopulation = function(data){
 	melted = data.table::melt(data, id.vars = c('Day', 'SeasonNumber'),
 		variable.name = 'GooseType', value.name = 'Numbers')
 	melted = melted[Numbers != 0,]
-	
+	if(dates) {
+	melted[, Day:=as.Date(Day, origin = '2010-01-01')]
+	}
 	p = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) + 
 		ggplot2::geom_line(ggplot2::aes(colour = GooseType)) + 
 		ggplot2::geom_point(ggplot2::aes(colour = GooseType)) + 
