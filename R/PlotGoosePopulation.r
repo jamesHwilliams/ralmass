277f1c272b93ee74f48633e6a52836cfc5be9176
LDalby <lars@bios.au.dk>
2016-06-21 14:05:39 +0200

    Fix dates on the x-axis

--- R/PlotGoosePopulation.r
+++ R/PlotGoosePopulation.r
@@ -1,7 +1,7 @@
 #' Plot goose population data
 #'
 #' Plot the number of geese per species and type
-#' 
+#'
 #' @param data data.table The ALMaSS data file GoosePopulationData.txt
 #' @param dates logical Should we plot day numbers as dates?
 #' @return A nice plot
@@ -13,10 +13,15 @@ PlotGoosePopulation = function(data, dates = FALSE){
 	if(dates) {
 	melted[, Day:=as.Date(Day, origin = '2010-01-01')]
 	}
-	p = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) + 
-		ggplot2::geom_line(ggplot2::aes(colour = GooseType)) + 
-		ggplot2::geom_point(ggplot2::aes(colour = GooseType)) + 
+	p = ggplot2::ggplot(melted, ggplot2::aes(Day, Numbers, group = GooseType)) +
+		ggplot2::geom_line(ggplot2::aes(colour = GooseType)) +
+		ggplot2::geom_point(ggplot2::aes(colour = GooseType)) +
 		ggplot2::ylab('Numbers') + ggplot2::theme_bw()
+	if(dates) {
+	  p = p + ggplot2::scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+	    ggplot2::xlab('Month')
+	}
+
 	return(p)
 }
 
