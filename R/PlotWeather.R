322d953b57b56311c483687f57c771809760c548
LDalby <lars@bios.au.dk>
2016-07-13 13:54:25 +0200

    ggplot in plotweather + impro subset

--- R/PlotWeather.R
+++ R/PlotWeather.R
@@ -2,31 +2,34 @@
 #'
 #' Plot an Almass weather file or a subset of it 
 #' 
+#' If from or to are left blank the entire range of data is plotted
+#' 
 #' @param file character The full path to the weather file
-#' @param subset numeric Either a year or a sequence of years. If left blank - plot the whole thing.
+#' @param from character Date in format year-month-day
+#' @param to character Date in format year-month-day
 #' @return A nice plot
 #' @export
-PlotWeather = function(file, subset = NULL){
+PlotWeather = function(file, from = NULL, to = NULL){
 	weather = data.table::fread(file, skip = 1)
-	setnames(weather, c('Year', 'Month', 'Day', 'Temp', 'Wind', 'Rain'))
-	weather[,Julian:=lubridate::ymd(paste(weather[,Year],weather[,Month],weather[,Day], sep = '-'))]
-	if(!is.null(subset)) 
+	setnames(weather, c('Year', 'Month', 'Day', 'Temperature', 'Wind', 'Rain'))
+	weather[,Date:=paste(weather[,Year],weather[,Month],weather[,Day], sep = '-')]
+	weather[,Date:=lubridate::ymd(Date)]
+	weather[, c('Year', 'Month', 'Day'):=NULL]
+	molten = data.table::melt(weather, measure.vars = c('Temperature', 'Wind', 'Rain'))
+	if(all(!is.null(from), !is.null(to))) 
 	{
-	if(min(subset) < min(weather[,Year]) | max(subset) > max(weather[,Year]))
+	minsubset = as.Date(from)
+	maxsubset = as.Date(to)
+	if(minsubset < molten[,min(Date)] | maxsubset > molten[,max(Date)])
 	{
-		return(cat('Subset out of range of the provided weather file\n'))
+		return(cat('Subset out of range of the provided weaather file\n'))
 	}
-		if(length(subset) > 1) 
-		{
-			weather = weather[lubridate::year(Julian) >= min(subset) & lubridate::year(Julian) <= max(subset)]
-		}
-		else
-		{
-			weather = weather[lubridate::year(Julian) == subset]
-		}
+	molten = molten[Date %between% c(minsubset, maxsubset)]
 	}
-	par(mfrow = c(3,1), las = 1, bty = 'l', mar =  c(2, 4, 2, 1) + 0.1)
-	plot(weather[,Julian], weather[,Temp], type = 'l', ylab = 'Temperature')
-	plot(weather[,Julian], weather[,Rain], type = 'l', ylab = 'Rain')
-	plot(weather[,Julian], weather[,Wind], type = 'l', ylab = 'Wind')
+	p = ggplot2::ggplot(molten, ggplot2::aes(Date, value)) +
+		ggplot2::geom_line() +
+		ggplot2::facet_grid(~variable, scales = 'free_y') +
+		ggplot2::scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+		ggplot2::theme_bw()
+	return(p)
 }
